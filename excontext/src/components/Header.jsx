b2b08957f99ed123d5168d2f0da6ef9e11d1f326
Viviane Rezende <vivianerezende95@gmail.com>
2025-10-29 23:28:50 -0300

    Sincroniza├º├úo autom├ítica via Jenkins

--- excontext/src/components/Header.jsx
+++ excontext/src/components/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => {
     }}>
         <h1>Meu App em Context API / Pipeline com Jenkins</h1>
         <p>Executando Pipeline a cada 5 minutos</p>
+        <p>Executando Pipeline a cada 5 minutos e fazendo commit a cada alteração</p>
     </header>
   )
 }
